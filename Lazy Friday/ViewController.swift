dfa76df937910cd19c8c64c1f00887024a8b96bf
Laura Sirvent <lsirvent@themobilebakery.com>
2016-02-19 16:16:07 +0100

    Initial Commit

---
+++ Lazy Friday/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lazy Friday
+//
+//  Created by bakerydev004 on 19/2/16.
+//  Copyright © 2016 Laura Sirvent Collado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
